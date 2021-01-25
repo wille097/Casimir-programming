7e9454e67e228a680efa8efddf6bb8c4f512c2f1
JoeriWillet <joeri.willet@wur.nl>
2021-01-25 13:38:49 +0100

    printin issue

--- script.py
+++ script.py
@@ -2,4 +2,4 @@ print('this is for circles')
 import test
 
 a = test.area_circle(10)
-prting(a)area _circle
+print(a)
--- test.py
+++ test.py
@@ -1,3 +1,9 @@
 
 print('Hello World!')
+import numpy as np
 
+def circumference_circle(radius):
+	return 2*np.pi*radius
+
+def area _circle(radius)
+	return np.pi*radius**2
